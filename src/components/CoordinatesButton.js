f486b2d29e3d709242b8204198bb171c769fd998
congo-quinine <congo.quinine@gmail.com>
2018-12-14 11:13:53 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,41 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+class CoordinatesButton extends React.Component {
+
+  handleClick = (event) => {
+    const mouseX = event.pageX
+    const mouseY = event.pageY
+    const coordinatesArray = [mouseX, mouseY]
+    this.props.onReceiveCoordinates(coordinatesArray)
+  }
+
+  render () {
+    return (
+      <button type='button' onClick={this.handleClick}></button>
+    )
+  }
+
+}
+
+export default CoordinatesButton
+
+
+
+
+
+// #### `CoordinatesButton`
+//
+// 1. In the `components/CoordinatesButton.js` file, create a `CoordinatesButton` React component.
+//
+// 2. This component takes in one prop: `onReceiveCoordinates`. This prop is a
+// _function_ passed down from `index.js`. This function is currently just logging
+// whatever is passed into it.
+//
+// 3. Within `CoordinatesButton`, render a button. On click of the button,
+// create an array with two elements: the X and Y coordinates of the mouse. Find
+// these using the event data.
+//
+// 4. Pass this event data in as the argument for the `onReceiveCoordinates` prop.
+//
+// 5. If successful, the current x,y position of your mouse should be logged.
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,37 @@
 // Code DelayedButton Component Here
+
+import React from 'react'
+
+class DelayedButton extends React.Component {
+
+  handleClick = (event) => {
+    event.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  }
+
+  render () {
+    return (
+        <button type='button' onClick={this.handleClick}></button>
+    )
+  }
+
+}
+
+export default DelayedButton
+
+
+
+// #### `DelayedButton`
+//
+// 1. In the `components/DelayedButton.js` file, create a `DelayedButton` React component
+//
+// 2. This component takes two props: `onDelayedClick` (a function), and `delay` (a
+// number).
+//
+// 3. Create a button that, when clicked, will pass the click event to the
+// `onDelayedClick` prop _within_ a `setTimeout()`. The `setTimeout()`
+// should be set to `this.props.delay`.
+//
+// 4. If successful, the event will be logged to the console once the timeout has finished.
